3a3038de6c884f974ea275900935e1ee106de89f
Gabriela Stanciu <elena-gabriela.stanciu@student.tuiasi.ro>
2026-04-15 19:40:53 +0300

    Feature face login + REST-API
    * Face Recognition Module completed
    
    * Rename face_recognition to face_recognition module + update files + REST API completed

---
+++ face_recognition_module/login.py
@@ -0,0 +1,50 @@
+import numpy as np
+from storage import load_users
+from register import get_live_embedding
+
+
+#vom compara embedding ul extras live cu embeddings urile din json, cea mai mica
+#diferenta de embeddings va rezulta emailul userului care e in proces de login
+
+def compute_difference(embedding1, embedding2):
+  return np.linalg.norm(embedding1 - embedding2)
+
+
+def find_best_match(live_embedding, threshold = 0.6, json_path = "users.json"):
+  data = load_users(json_path)
+
+  if len(data["users"]) == 0:
+    print("Nu exista users salvati in fisier")
+    return None
+  best_email = None
+  best_difference = float("inf")
+
+  for user in data["users"]:
+    saved_embedding = np.array(user["embedding"])
+    difference = compute_difference(live_embedding, saved_embedding)
+
+    if difference < best_difference:
+      best_difference = difference
+      best_email = user["email"]
+  if best_difference < threshold:
+    return best_email
+
+  return None
+
+
+def login_user():
+  live_embedding = get_live_embedding()
+
+  if live_embedding is None:
+    print("Login esuat: nu s a putut obtine embedding ul")
+    return None
+
+  matched_email = find_best_match(live_embedding)
+
+  if matched_email is None:
+    print("Login esuat: utilizator nerecunoscut")
+    return None
+
+  print(f"Login reusit: {matched_email}")
+  return matched_email
+  
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.proiectpip2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens org.example.proiectpip2 to javafx.fxml;
-    exports org.example.proiectpip2;
-}
